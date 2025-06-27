0b9393a2aa10b1283f13c1aa07dacac3bf367d6b
a.gonnykh <a.gonnykh@tbank.ru>
2025-06-27 23:01:00 +0300

    fix elastic

--- init-index.sh
+++ init-index.sh
@@ -5,15 +5,34 @@ curl -X PUT "http://elasticsearch:9200/products" -H 'Content-Type: application/j
   "settings": {
     "analysis": {
       "filter": {
+        "russian_stop": {
+          "type":       "stop",
+          "stopwords":  "_russian_"
+        },
+        "ru_hunspell": {
+          "type":   "hunspell",
+          "locale": "ru_RU",
+          "dedup":  true
+        },
         "ru_synonyms": {
-          "type": "synonym",
+          "type":          "synonym_graph",
           "synonyms_path": "synonyms/ru_synonyms.txt"
+        },
+        "russian_stemmer": {
+          "type":     "stemmer",
+          "language": "russian"
         }
       },
       "analyzer": {
-        "ru_synonym_analyzer": {
+        "ru_product_analyzer": {
           "tokenizer": "standard",
-          "filter": ["lowercase", "ru_synonyms"]
+          "filter": [
+            "lowercase",
+            "russian_stop",
+            "ru_hunspell",
+            "ru_synonyms",
+            "russian_stemmer"
+          ]
         }
       }
     }
@@ -21,11 +40,11 @@ curl -X PUT "http://elasticsearch:9200/products" -H 'Content-Type: application/j
   "mappings": {
     "properties": {
       "name": {
-        "type": "text",
-        "analyzer": "ru_synonym_analyzer"
+        "type":     "text",
+        "analyzer": "ru_product_analyzer"
       },
       "unit": {
-        "type": "keyword"
+        "type":     "keyword"
       }
     }
   }
